900ca1d25eb916c31f47f342f19eb9c6c3fba5e9
jin02s <143775953+jin02s@users.noreply.github.com>
2023-09-01 16:13:43 +0900

    Delete day05 directory

--- day05/src/util/convertFunc.js
+++
@@ -1,8 +0,0 @@
-const convertMMDDYYYY = (val) => {
-  if (typeof val == "number") {
-    return new Date(val).toLocaleDateString("en-US");
-  }
-  return `${val.toLocaleDateString("en-US")}`;
-};
-
-export default convertMMDDYYYY;
